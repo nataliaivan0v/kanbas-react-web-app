70a7cd10de07a57c9942ed2e7b5942689fb2ce7d
nataliaivan0v <nataliaivanov2004@gmail.com>
2025-01-09 15:02:28 -0500

    add screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import Labs from "./Labs";
-import { HashRouter, Navigate, Route, Routes } from "react-router";
-function App() {
-  return (
-    <HashRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Navigate to="/Labs" />} />
-          <Route path="/Labs/*" element={<Labs />} />
-          <Route path="/Kambaz" element={<h1>Kambaz</h1>} />
-          <Route path="/Project" element={<h1>Project</h1>} />
-        </Routes>
-      </div>
-    </HashRouter>
-  );
-}
+import Kambaz from "./Kambaz";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 
-export default App;
-
+export default function App() {
+ return (
+  <HashRouter>
+   <div>
+    <Routes>
+     <Route path="/" element={<Navigate to="Kambaz"/>}/>
+     <Route path="/Labs/*" element={<Labs />} />
+     <Route path="/Kambaz/*" element={<Kambaz />} />
+    </Routes>
+   </div>
+  </HashRouter>
+);}
