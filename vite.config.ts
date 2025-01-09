114116dfe521c3b7b18ff836e8d27acf0750b56c
nataliaivan0v <nataliaivanov2004@gmail.com>
2025-01-09 11:16:08 -0500

    undo

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Load environment variables
-const API_URL = process.env.VITE_API_URL || "https://default-api.example.com";
-const APP_ENV = process.env.VITE_APP_ENV || "development";
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __DEFINES__: JSON.stringify({
-      API_URL,
-      APP_ENV,
-    }),
-  },
-});
+})
