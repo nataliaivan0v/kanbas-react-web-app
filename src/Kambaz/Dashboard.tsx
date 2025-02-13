37616400e9527b1c217412f67908c5ef411421fb
nataliaivan0v <nataliaivanov2004@gmail.com>
2025-02-13 16:45:50 -0500

    database stuff

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,184 +1,48 @@
 import { Link } from "react-router-dom";
 import { Row, Col, Card, Button } from "react-bootstrap";
+import * as db from "./Database";
 
 export default function Dashboard() {
+  const courses = db.courses;
+
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
+      <h2 id="wd-dashboard-published">
+        Published Courses ({courses.length})
+      </h2>{" "}
+      <hr />
       <div id="wd-dashboard-courses">
         <Row xs={1} md={5} className="g-4">
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/react.png"
-                  width="100%"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS1234 React JS
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Full Stack software developer
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/class2.jpg"
-                  width="200"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS 2500
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Fundies 1
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/class3.jpeg"
-                  width="200"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS 2510
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Fundies 2
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/class5.png"
-                  width="200"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS 3500
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Object-Oriented Design
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-        </Row>
-        <br></br>
-        <Row xs={1} md={5} className="g-4">
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/class6.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS 3000
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Algorithms and Data
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/class4.webp"
-                  width="200"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS 3650
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Computer Systems
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
-              <Link
-                to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <Card.Img
-                  variant="top"
-                  src="/images/class7.jpeg"
-                  width="200"
-                  height={160}
-                />
-                <Card.Body>
-                  <Card.Title className="wd-dashboard-course-title">
-                    CS 3800
-                  </Card.Title>
-                  <Card.Text className="wd-dashboard-course-description">
-                    Theory of Computation
-                  </Card.Text>
-                  <Button variant="primary">Go</Button>
-                </Card.Body>
-              </Link>
-            </Card>
-          </Col>
+          {courses.map((course) => (
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  to={`/Kambaz/Courses/${course._id}/Home`}
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <Card.Img
+                    src="/images/react.png"
+                    variant="top"
+                    width="100%"
+                    height={160}
+                  />
+                  <Card.Body className="card-body">
+                    <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      {course.name}{" "}
+                    </Card.Title>
+                    <Card.Text
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      {course.description}{" "}
+                    </Card.Text>
+                    <Button variant="primary"> Go </Button>
+                  </Card.Body>
+                </Link>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </div>
     </div>
--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -3,85 +3,70 @@ import { FaRegCircleUser, FaInbox } from "react-icons/fa6";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { IoCalendarOutline } from "react-icons/io5";
+import { ListGroup } from "react-bootstrap";
 
 export default function KambazNavigation() {
-  const location = useLocation();
-
-  const getLinkClass = (path: string) =>
-    location.pathname === path
-      ? "list-group-item text-center border-0 bg-white text-danger"
-      : "list-group-item text-white bg-black text-center border-0";
+  const { pathname } = useLocation();
+  const links = [
+    { label: "Dashboard", path: "/Kambaz/Dashboard", icon: AiOutlineDashboard },
+    { label: "Courses", path: "/Kambaz/Dashboard", icon: LiaBookSolid },
+    { label: "Calendar", path: "/Kambaz/Calendar", icon: IoCalendarOutline },
+    { label: "Inbox", path: "/Kambaz/Inbox", icon: FaInbox },
+    { label: "Labs", path: "/Labs", icon: LiaCogSolid },
+  ];
 
   return (
-    <div
+    <ListGroup
       id="wd-kambaz-navigation"
-      style={{ width: 110 }}
-      className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+      style={{ width: 120 }}
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
     >
-      <br />
-      <a
+      <br></br>
+      <ListGroup.Item
         id="wd-neu-link"
         target="_blank"
         href="https://www.northeastern.edu/"
-        className="list-group-item bg-black border-0 text-center"
+        action
+        className="bg-black border-0 text-center"
       >
-        <img
-          src="/images/neu.webp"
-          width="75px"
-          alt="Northeastern University"
-        />
-      </a>
-      <br />
-      <Link
+        <img src="/images/neu.webp" width="75px" />
+      </ListGroup.Item>
+      <br></br>
+      <ListGroup.Item
+        as={Link}
         to="/Kambaz/Account"
-        id="wd-account-link"
-        className={getLinkClass("/Kambaz/Account")}
+        className={`text-center border-0 bg-black
+            ${
+              pathname.includes("Account")
+                ? "bg-white text-danger"
+                : "bg-black text-white"
+            }`}
       >
-        <FaRegCircleUser className="fs-1 text-white" />
+        <FaRegCircleUser
+          className={`fs-1 ${
+            pathname.includes("Account") ? "text-danger" : "text-white"
+          }`}
+        />
         <br />
         Account
-      </Link>
-      <Link
-        to="/Kambaz/Dashboard"
-        id="wd-dashboard-link"
-        className={getLinkClass("/Kambaz/Dashboard")}
-      >
-        <AiOutlineDashboard className="fs-1 text-danger" />
-        <br />
-        Dashboard
-      </Link>
-      <Link
-        to="/Kambaz/Courses"
-        id="wd-course-link"
-        className={getLinkClass("/Kambaz/Courses")}
-      >
-        <LiaBookSolid className="fs-1 text-danger" />
-        <br />
-        Courses
-      </Link>
-      <Link
-        to="/Kambaz/Calendar"
-        id="wd-calendar-link"
-        className={getLinkClass("/Kambaz/Calendar")}
-      >
-        <IoCalendarOutline className="fs-1 text-danger" />
-        <br />
-        Calendar
-      </Link>
-      <Link
-        to="/Kambaz/Inbox"
-        id="wd-inbox-link"
-        className={getLinkClass("/Kambaz/Inbox")}
-      >
-        <FaInbox className="fs-1 text-danger" />
-        <br />
-        Inbox
-      </Link>
-      <Link to="/Labs" id="wd-labs-link" className={getLinkClass("/Labs")}>
-        <LiaCogSolid className="fs-1 text-danger" />
-        <br />
-        Labs
-      </Link>
-    </div>
+      </ListGroup.Item>
+      {links.map((link) => (
+        <ListGroup.Item
+          key={link.path}
+          as={Link}
+          to={link.path}
+          className={`bg-black text-center border-0
+              ${
+                pathname.includes(link.label)
+                  ? "text-danger bg-white"
+                  : "text-white bg-black"
+              }`}
+        >
+          {link.icon({ className: "fs-1 text-danger" })}
+          <br />
+          {link.label}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   );
 }
