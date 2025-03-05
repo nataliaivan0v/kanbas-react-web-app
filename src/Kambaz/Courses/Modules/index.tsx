287c097f6afd0ccfa6c398142a0301612166fc18
nataliaivan0v <nataliaivanov2004@gmail.com>
2025-03-05 15:09:30 -0500

    up until 4.4.3.3

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -4,14 +4,31 @@ import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 import { useParams } from "react-router";
 import * as db from "../../Database";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Modules() {
   const { cid } = useParams();
-  const modules = db.modules;
+  const [modules, setModules] = useState<any[]>(db.modules); // eslint-disable-line @typescript-eslint/no-explicit-any
+  const [moduleName, setModuleName] = useState("");
+  const addModule = () => {
+    setModules([
+      ...modules,
+      { _id: uuidv4(), name: moduleName, course: cid, lessons: [] },
+    ]);
+    setModuleName("");
+  };
+  const deleteModule = (moduleId: string) => {
+    setModules(modules.filter((m) => m._id !== moduleId));
+  };
 
   return (
     <div>
-      <ModulesControls />
+      <ModulesControls
+        setModuleName={setModuleName}
+        moduleName={moduleName}
+        addModule={addModule}
+      />
       <br />
       <br />
       <br />
@@ -20,15 +37,26 @@ export default function Modules() {
         {modules
           .filter((module) => module.course === cid)
           .map((module) => (
-            <li key={module.name} className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <li
+              key={module.name}
+              className="wd-module list-group-item p-0 mb-5 fs-5 border-gray"
+            >
               <div className="wd-title p-3 ps-2 bg-secondary">
-                <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons />
+                <BsGripVertical className="me-2 fs-3" /> {module.name}{" "}
+                <ModuleControlButtons
+                  moduleId={module._id}
+                  deleteModule={deleteModule}
+                />
               </div>
               {module.lessons && (
                 <ul className="wd-lessons list-group rounded-0">
                   {module.lessons.map((lesson) => (
-                    <li key={lesson.name} className="wd-lesson list-group-item p-3 ps-1">
-                      <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
+                    <li
+                      key={lesson.name}
+                      className="wd-lesson list-group-item p-3 ps-1"
+                    >
+                      <BsGripVertical className="me-2 fs-3" /> {lesson.name}{" "}
+                      <LessonControlButtons />
                     </li>
                   ))}
                 </ul>
