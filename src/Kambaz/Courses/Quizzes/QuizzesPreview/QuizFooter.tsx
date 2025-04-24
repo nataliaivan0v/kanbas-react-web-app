e24e1cc51acb4df3f8ac69b98a7088130bea6468
DilanRocco <captaindilan12@gmail.com>
2025-04-24 12:51:44 -0400

    fix formatting

--- src/Kambaz/Courses/Quizzes/QuizzesPreview/QuizFooter.tsx
+++ src/Kambaz/Courses/Quizzes/QuizzesPreview/QuizFooter.tsx
@@ -12,7 +12,7 @@ export default function QuizScoreFooter({ score, maxScore, questionsRight, numOf
   return (
     <div className="mt-5">
       <div className="d-flex justify-content-end align-items-center">
-        <h5 className="fw-bold me-3">Score: {score}/{maxScore} {questionsRight} out of {numOfQuestions} questions. </h5>
+        <h5 className="fw-bold me-3">Score: {score}/{maxScore} points | {questionsRight}/{numOfQuestions} questions. </h5>
         <Button variant="secondary" onClick={onNext}>
           Next
         </Button>
